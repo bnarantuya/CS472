99ea80886f2932615bb78e875bac5d08890473cf
unknown <986989@CS.MUM.EDU>
2019-05-16 13:54:34 -0500

    mouse maze done

---
+++ Javascript Labs/Mouse Maze Lab/Maze!_files/maze.js
@@ -0,0 +1,51 @@
+console.log("loaded");
+
+var isStarted = false;
+var isWon = false;
+$(document).ready(function () {
+    events.gameStart();
+});
+
+
+var events = {
+    touchBoundary: function () {
+        $('.boundary').mouseover(function () {
+            if(isStarted) {
+                $('.boundary').addClass('youlose');
+                alert('Sorry, you lost :[');
+                isStarted = false;
+            }
+        });
+
+    },
+    startAgain: function () {
+        $("body").keydown(function (event) {
+            if (event.which == 83 && isStarted == false) {
+                $('.boundary').removeClass('youlose');
+                isStarted = true;
+                isWon = false;
+            }
+        });
+    },
+
+    gameStart: function() {
+        $('#start').click(function() {
+            events.touchBoundary();
+            events.startAgain();
+            events.winCondition();
+            $('.boundary').removeClass('youlose');
+            isStarted = true;
+        });
+    },
+
+    winCondition: function() {
+        $('#end').mouseover(function() {
+            if(!isWon) {
+                alert('You win :]');
+                isStarted = false;
+                isWon = true;
+            }
+
+        });
+    }
+}
